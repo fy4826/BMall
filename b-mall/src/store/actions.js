95d082c233fbbc6c99aae033b2aa336126de8e02
carbide <15867139271@163.com>
2020-12-20 18:55:40 +0800

    1 commit

--- b-mall/src/store/actions.js
+++
@@ -1,8 +0,0 @@
-export default {
-    saveUserName(context, username) {
-        context.commit('saveUserName', username)
-    },
-    saveCartCount(context, count) {
-        context.commit('saveCartCount', count)
-    }
-}
--- b-mall/src/store/mutations.js
+++
@@ -1,8 +0,0 @@
-export default {
-    saveUserName(state, username) {
-        state.username = username;
-    },
-    saveCartCount(state, count) {
-        state.cartCount = count;
-    }
-}
